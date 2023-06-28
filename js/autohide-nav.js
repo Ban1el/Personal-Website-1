4000c55aa9486817382fd10c0b6265ee61fcc44c
BBabUwU <106431292+BBabUwU@users.noreply.github.com>
2023-06-28 17:28:57 +0800

    Fixed navigation auto-hide for mobile users

--- js/autohide-nav.js
+++ js/autohide-nav.js
@@ -8,7 +8,7 @@ if (el_autohide) {
   window.addEventListener("scroll", function () {
     const scroll_top = window.scrollY;
 
-    if (scroll_top < last_scroll_top - offset) {
+    if (scroll_top < last_scroll_top - offset || scroll_top <= 0) {
       el_autohide.classList.remove("scrolled-down");
       el_autohide.classList.add("scrolled-up");
     } else if (scroll_top > last_scroll_top + offset) {
